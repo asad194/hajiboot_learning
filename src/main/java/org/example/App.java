24cf127eff857bf008b39db498c5764cedf23cdc
asad194 <a19653399>
2022-06-05 09:49:48 +0900

    P17まで。アプリケーション実行とHelloWorld出力

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // コントローラクラスであることを示す。
+@EnableAutoConfiguration // 設定の自動化
+public class App
 {
+    @RequestMapping("/")    // URLが"/"であるアクセスとの紐付け
+    String home() {
+        return "Hello world!";  // RestController
+        // アノテーションの付いたクラスのメソッドで文字列を返す＝返り値をHTTPとして出力
+ }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
